85bd2c8c8d13857e747c23042fffedabcda60e99
Paul Cuciureanu <paulcu@gmail.com>
2017-01-02 23:36:17 -0500

    Switching to 2 spaces indents

--- async/geolocation/src/fetch.js
+++ async/geolocation/src/fetch.js
@@ -1,10 +1,12 @@
 var fetchLocation = function(onSuccess, onError) {
   var returnLocation = function(position) {
-    var location = { 
-      lat: position.coords.latitude, lon: position.coords.longitude };
+    var location = {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude
+    };
 
     onSuccess(location);
   };
-  
+
   navigator.geolocation.getCurrentPosition(returnLocation, onError);
 };
