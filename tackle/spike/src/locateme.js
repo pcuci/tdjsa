65b1a701672eba5e88813ee68bda623b55f7cbff
Paul Cuciureanu <paulcu@gmail.com>
2017-01-03 19:53:46 -0500

    Adding geolocate spike

--- tackle/spike/src/locateme.js
+++ tackle/spike/src/locateme.js
@@ -1,13 +1,14 @@
 var locate = function() {
-    navigator.geolocation.getCurrentPosition(
-        function(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
+  navigator.geolocation.getCurrentPosition(
+    function(position) {
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
 
-            var url = 'https://maps.google.com/?q=' + latitude + ',' + longitude;
-            window.location = url;
-        },
-        function() {
-            document.getElementById('error').innerHTML = 'unable to get your location';
-        });
+      var url = 'http://maps.google.com/?q=' + latitude + ',' + longitude;
+      window.location = url;
+    },
+    function() {
+      document.getElementById('error').innerHTML = 'unable to get your location';
+    }
+  );
 };
