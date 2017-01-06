8123d443f59b57f72265724b2f7b2d49f646a9e9
Paul Cuciureanu <paulcu@gmail.com>
2017-01-06 00:08:29 -0500

    Fixing all tests and code for stockfetch

--- testnode/stockfetch/src/stockfetch-driver.js
+++ testnode/stockfetch/src/stockfetch-driver.js
@@ -1,15 +1,19 @@
 var Stockfetch = require('./stockfetch');
 
-var onError = function(error) { console.log(error) };
-
 var display = function(prices, errors) {
-    var print = function(data) { console.log(data[0] + '\t' + data[1]); };
+  var print = function(data) {
+    console.log(data[0] + '\t' + data[1]);
+  };
 
-    console.log("Prices for ticker symbols:");
-    prices.forEach(print);
+  console.log("Prices for ticker symbols:");
+  prices.forEach(print);
 
-    console.log("Ticker symbols with error:");
-    errors.forEach(print);
+  console.log("Ticker symbols with errors:");
+  errors.forEach(print);
+};
+
+var onError = function(error) {
+  console.error(error);
 };
 
 new Stockfetch().getPriceForTickers('mixedTickers.txt', display, onError);
--- testnode/stockfetch/src/stockfetch-service.js
+++ testnode/stockfetch/src/stockfetch-service.js
@@ -3,21 +3,21 @@ var querystring = require('querystring');
 var StockFetch = require('./stockfetch');
 
 var handler = function(req, res) {
-    var symbolString = querystring.parse(req.url.split('?')[1]).s || '';
+  var symbolString = querystring.parse(req.url.split('?')[1]).s || '';
 
-    if(symbolString !== '') {
-        var stockfetch = new StockFetch();
-        var tickers = symbolString.split(',');
+  if(symbolString !== '') {
+    var stockfetch = new StockFetch();
+    var tickers = symbolString.split(',');
 
-        stockfetch.reportCallback = function(prices, errors) {
-            res.end(JSON.stringify({prices: prices, errors: errors}));
-        };
+    stockfetch.reportCallback = function(prices, errors) {
+      res.end(JSON.stringify({prices: prices, errors: errors}));
+    };
 
-        stockfetch.processTickers(tickers);
-    }
-    else {
-        res.end('invalid query, use format ?=sSYM1,SYM2');
-    }
+    stockfetch.processTickers(tickers);
+  }
+  else {
+    res.end('invalid query, use format ?=sSYM1,SYM2');
+  }
 };
 
 http.createServer(handler).listen(3001);
