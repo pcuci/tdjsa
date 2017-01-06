5c1b50ecdbfa38c0cbd41900296105d8e6e48072
Paul Cuciureanu <paulcu@gmail.com>
2017-01-06 00:32:39 -0500

    Providing http access

--- testnode/stockfetch/src/stockfetch-service.js
+++ testnode/stockfetch/src/stockfetch-service.js
@@ -5,7 +5,7 @@ var StockFetch = require('./stockfetch');
 var handler = function(req, res) {
   var symbolString = querystring.parse(req.url.split('?')[1]).s || '';
 
-  if(symbolString !== '') {
+  if (symbolString !== '') {
     var stockfetch = new StockFetch();
     var tickers = symbolString.split(',');
 
@@ -14,10 +14,12 @@ var handler = function(req, res) {
     };
 
     stockfetch.processTickers(tickers);
-  }
-  else {
-    res.end('invalid query, use format ?=sSYM1,SYM2');
+  } else {
+    res.end('invalid query, use format ?s=SYM1,SYM2');
   }
 };
 
 http.createServer(handler).listen(3001);
+
+/* TODO To write automated tests you’d have to design the code in stockfetch-service.js file differently. First, move the handler function to a separate file so it can be isolated and tested. The tests would verify that the function is handling various query strings, like an empty query string or one with no symbols, and so on. To test these we’d stub out req and res. Then the tests can verify that the function is interacting correctly with Stockfetch—we’d have to pass Stockfetch as a parameter for easy stubbing. Finally, we can test that the service file registers the handler function properly with the http.createServer function. That would test this piece of code, but in reality a typical web app would use different HTTP methods, like GET, POST, and DELETE, and multiple routes, like /, /stocks, and so on.
+*/
